9c75d1477831ed1cacaef628c00d9ba21b9482a6
Ashlen Kurre <157025512+alkurre@users.noreply.github.com>
2026-05-06 12:02:20 +0200

    fix tests

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -2,7 +2,6 @@ import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
 
-// cleanup after each test case
 afterEach(() => {
   cleanup();
 });
